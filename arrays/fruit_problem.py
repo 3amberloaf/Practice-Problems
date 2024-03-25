522e30ba70cc8fa9ef632dbba0dd557f561116db
Amber Sautner <ambersautner3@gmail.com>
2024-03-25 13:46:56 -0400

    practicing

---
+++ arrays/fruit_problem.py
@@ -0,0 +1,5 @@
+# In a game called Fruit Crush, you can choose two dissimilar fruits and crush them. Each fruit is represented by an integer in an array. 
+# Given an array 'Fruit' of size n, return the minimum number of fruits left after a given operation is performed a number of times. 
+
+# Constraints: 1 <= n <= 10^5
+
